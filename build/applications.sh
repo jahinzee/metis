3f99520d269a9d92dd3853dd34f385f1dcc8d2ac
jahinzee <jahinzee@proton.me>
2025-10-31 12:07:17 +1100

    chore: formatting

--- build/applications.sh
+++ build/applications.sh
@@ -14,50 +14,53 @@ remove ublue-os-just
 
 # ››› Desktop applications
 #
-install elisa \
-        gwenview \
-        haruna \
-        kalk \
-        kamoso \
-        kclock \
-        kcm_systemd \
-        kcolorchooser \
-        kolourpaint \
-        krdc \
-        ksystemlog \
-        lm_sensors \
-        merkuro \
-        okular \
-        plasma-browser-integration \
-        yakuake 
+install \
+    elisa \
+    gwenview \
+    haruna \
+    kalk \
+    kamoso \
+    kclock \
+    kcm_systemd \
+    kcolorchooser \
+    kolourpaint \
+    krdc \
+    ksystemlog \
+    lm_sensors \
+    merkuro \
+    okular \
+    plasma-browser-integration \
+    yakuake 
 
 # ››› Command-line and core applications
 #
 # INFO: `eza` is not available in the stock Fedora repos, so we're installing from a COPR.
 #       See: <https://github.com/eza-community/eza/blob/main/INSTALL.md#fedora>
-enable-coprs atim/starship \
-             dturner/eza \
-             lilay/topgrade
-install bat \
-        btop \
-        distrobox \
-        eza \
-        fastfetch \
-        fd \
-        fish \
-        fortune-mod \
-        helix \
-        hyperfine \
-        pipx \
-        podman-docker \
-        podman-compose \
-        qalculate \
-        ripgrep \
-        starship \
-        topgrade \
-        vim \
-        xlsclients \
-        zoxide
+enable-coprs \
+    atim/starship \
+    dturner/eza \
+    lilay/topgrade
+install \
+    bat \
+    btop \
+    distrobox \
+    eza \
+    fastfetch \
+    fd \
+    fish \
+    fortune-mod \
+    helix \
+    hyperfine \
+    pipx \
+    podman-docker \
+    podman-compose \
+    qalculate \
+    ripgrep \
+    starship \
+    topgrade \
+    vim \
+    xlsclients \
+    zoxide
 
 # ››› Virtualisation (libvirt/QEMU/KVM)
 #
@@ -67,10 +70,10 @@ install @virtualization
 #
 # enable-coprs badshah/openbangla-keyboard
 install \
-fcitx5 \
-        fcitx5-mozc \
-        kcm-fcitx5 \
-#         fcitx-openbangla
+    fcitx5 \
+    fcitx5-mozc \
+    kcm-fcitx5 \
+    # fcitx-openbangla
 # TEMPORARY: Using the F42 build of OpenBangla until packaging is ready for Fedora 43.
 install "https://download.copr.fedorainfracloud.org/results/badshah/openbangla-keyboard/fedora-42-x86_64/09018418-fcitx-openbangla/fcitx-openbangla_3.0.0-.rpm"
 
@@ -78,18 +81,20 @@ install "https://download.copr.fedorainfracloud.org/results/badshah/openbangla-k
 #
 # INFO: Since Homebrew is a user-level tool, integrating it on the system layer doesn't make a lot
 #       of sense, but installing the dependencies is fine.
-install @development-tools \
-        procps-ng \
-        curl \
-        file
+install \
+    @development-tools \
+    procps-ng \
+    curl \
+    file
 
 # ››› Syncthing and SyncthingTray (Plasmoid/KIO/CLI)
 #
 enable-obs-repos home:mkittler
-install syncthing \
-        syncthingplasmoid-qt6 \
-        syncthingfileitemaction-qt6 \
-        syncthingctl-qt6
+install \
+    syncthing \
+    syncthingplasmoid-qt6 \
+    syncthingfileitemaction-qt6 \
+    syncthingctl-qt6
 
 # ››› Printing (brlaser)
 #
